6664592010fb3150396751ec143e39c423cf2930
nate <nate@nateui-MacBookAir.local>
2024-08-11 13:02:01 +0900

    update

--- gallery.js
+++ gallery.js
@@ -3,36 +3,36 @@ window.onload = () => {
   const collapseBtn = document.getElementById("collapseBtn");
   const hiddenImages = document.querySelectorAll("#gallery img.hidden");
 
-  showMoreBtn.addEventListener("click", () => {
-    hiddenImages.forEach((img) => {
-      img.classList.remove("hidden");
-    });
-    collapseBtn.style.display = "inline-block";
-    collapseBtn.style.backgroundColor = "ffe08c";
-    collapseBtn.style.width = "70px";
-    collapseBtn.style.textAlign = "center";
-    collapseBtn.style.display = "block";
-    collapseBtn.style.margin = "7% auto";
-    collapseBtn.style.borderRadius = "15px";
+  // showMoreBtn.addEventListener("click", () => {
+  //   hiddenImages.forEach((img) => {
+  //     img.classList.remove("hidden");
+  //   });
+  //   collapseBtn.style.display = "inline-block";
+  //   collapseBtn.style.backgroundColor = "ffe08c";
+  //   collapseBtn.style.width = "70px";
+  //   collapseBtn.style.textAlign = "center";
+  //   collapseBtn.style.display = "block";
+  //   collapseBtn.style.margin = "7% auto";
+  //   collapseBtn.style.borderRadius = "15px";
 
-    showMoreBtn.style.display = "none";
-  });
+  //   showMoreBtn.style.display = "none";
+  // });
 
-  collapseBtn.addEventListener("click", () => {
-    const galleryImages = document.querySelectorAll("#addGalleyItem");
-    galleryImages.forEach((img) => {
-      img.classList.add("hidden");
-    });
-    collapseBtn.style.display = "none";
+  // collapseBtn.addEventListener("click", () => {
+  //   const galleryImages = document.querySelectorAll("#addGalleyItem");
+  //   galleryImages.forEach((img) => {
+  //     img.classList.add("hidden");
+  //   });
+  //   collapseBtn.style.display = "none";
 
-    showMoreBtn.style.display = "inline-block";
-    showMoreBtn.style.backgroundColor = "ffe08c";
-    showMoreBtn.style.width = "70px";
-    showMoreBtn.style.textAlign = "center";
-    showMoreBtn.style.display = "block";
-    showMoreBtn.style.margin = "7% auto";
-    showMoreBtn.style.borderRadius = "15px";
-  });
+  //   showMoreBtn.style.display = "inline-block";
+  //   showMoreBtn.style.backgroundColor = "ffe08c";
+  //   showMoreBtn.style.width = "70px";
+  //   showMoreBtn.style.textAlign = "center";
+  //   showMoreBtn.style.display = "block";
+  //   showMoreBtn.style.margin = "7% auto";
+  //   showMoreBtn.style.borderRadius = "15px";
+  // });
 
   // const galleryImages = document.querySelectorAll("#gallery img");
   // galleryImages.forEach((img) => {
@@ -115,5 +115,30 @@ window.onload = () => {
     const message = "모바일 청첩장을 확인해주세요! " + window.location.href;
     const link = "sms:?body=" + encodeURIComponent(message);
     window.location.href = link;
+  });
+
+  // BGM
+  const playButton = document.getElementById("playButton");
+  const audio = document.getElementById("bgm");
+  // 초기 상태는 play.png
+  playButton.src = "img/play.png";
+  playButton.addEventListener("click", function () {
+    if (audio) {
+      // 현재 오디오가 재생 중인지 여부를 확인
+      if (audio.paused) {
+        // 오디오가 멈춰있으면 재생
+        audio.play().catch((error) => {
+          console.error("오디오 재생 중 오류 발생:", error);
+        });
+        playButton.src = "img/stop.png"; // 버튼 이미지를 stop.png로 변경
+      } else {
+        // 오디오가 재생 중이면 정지
+        audio.pause();
+        audio.currentTime = 0; // 재생 위치를 처음으로 설정
+        playButton.src = "img/play.png"; // 버튼 이미지를 play.png로 변경
+      }
+    } else {
+      console.error("Audio element not found.");
+    }
   });
 };
