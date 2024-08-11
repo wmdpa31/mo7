d77630638820ef203386cb14874c8193c8554c8d
nate <nate@nateui-MacBookAir.local>
2024-08-11 12:28:47 +0900

    bgm update

--- bgm.js
+++ bgm.js
@@ -2,8 +2,8 @@ window.onload = () => {
   const playButton = document.getElementById("playButton");
   const audio = document.getElementById("bgm");
 
-  // 초기 상태는 play_start.png
-  playButton.src = "img/play_start.png";
+  // 초기 상태는 play.png
+  playButton.src = "img/play.png";
 
   playButton.addEventListener("click", function () {
     if (audio) {
@@ -13,12 +13,12 @@ window.onload = () => {
         audio.play().catch((error) => {
           console.error("오디오 재생 중 오류 발생:", error);
         });
-        playButton.src = "img/play_stop.png"; // 버튼 이미지를 play_stop.png로 변경
+        playButton.src = "img/stop.png"; // 버튼 이미지를 stop.png로 변경
       } else {
         // 오디오가 재생 중이면 정지
         audio.pause();
         audio.currentTime = 0; // 재생 위치를 처음으로 설정
-        playButton.src = "img/play_start.png"; // 버튼 이미지를 play_start.png로 변경
+        playButton.src = "img/play.png"; // 버튼 이미지를 play.png로 변경
       }
     } else {
       console.error("Audio element not found.");
